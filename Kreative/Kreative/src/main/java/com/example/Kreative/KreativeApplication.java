28bf86074e597fafb98e8ac39883166db8e566c3
kartik30R <kartik30rehan@gmail.com>
2026-02-04 11:03:06 +0530

    init

---
+++ Kreative/Kreative/src/main/java/com/example/Kreative/KreativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kreative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreativeApplication.class, args);
+	}
+
+}
---
+++ Kreative/Kreative/src/test/java/com/example/Kreative/KreativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kreative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
